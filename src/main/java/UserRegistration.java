0b2cb11dbfc3183ab0b0497028824c8a3eb396f4
prasadpalkar94 <prasadpalkar94@gmail.com>
2020-02-18 16:11:34 +0530

    ADDED:Email function for test

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -2,7 +2,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class UserRegistration {
     final static String name="^[A-Z]{1}[A-Za-z]{3,}$";
-    final static String email=
+    final static String email="^[a-zA-Z]*[-./+]?[0-9]*[@]{1}[a-z0-9]*[.]{1}[a-z]{3}$";
     public boolean checkFirstName(String firstName) {
         Pattern pattern = Pattern.compile(name);
         Matcher matcher = pattern.matcher(firstName);
@@ -15,15 +15,9 @@ public class UserRegistration {
         return matcher.matches();
     }
 
-    public static boolean checkLastName(String email) {
-        Pattern pattern = Pattern.compile(name);
+    public boolean checkEmail(String email) {
+        Pattern pattern = Pattern.compile(email);
         Matcher matcher = pattern.matcher(email);
-        return matcher.matches();l
+        return matcher.matches();
     }
-
-
-
-
-
-
 }
